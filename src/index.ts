56baab9eada9ad5b3a8224ce4582a4f5f4239d0b
mehrabix <mehrabi@post.com>
2024-10-29 23:27:32 +0330

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { UserBuilder } from "./UserBuilder.js";
-
-try {
-  const user = UserBuilder()
-    .setId("123e4567-e89b-12d3-a456-426614174000")
-    .setName("Alice")
-    .setAge(25)
-    .setEmail("alice@example.com")
-    .build();
-
-  console.log("User created:", user);
-} catch (error) {
-  console.error("Validation error:", error);
-}
+export { createBuilder } from './Builder.js';
